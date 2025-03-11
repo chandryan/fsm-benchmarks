49ce5c88c7c379d24cb8152e092ba4f4ccab30e4
Christian Granzin <christian.granzin@gmail.com>
2025-03-11 13:54:20 -0400

    first takeover

---
+++ bench/large/msm/Backmp11FavorCompileTime.cpp
@@ -0,0 +1,16 @@
+#define TRANSITION_TABLE_TYPE boost::mp11::mp_list
+#include <boost/mp11.hpp>
+#include "common.hpp"
+#include <boost/msm/backmp11/state_machine.hpp>
+#include <boost/msm/backmp11/favor_compile_time.hpp>
+
+using fsm0 = msm::back::state_machine<fsm_, msm::back::favor_compile_time>;
+
+int main()
+{
+    test_fsm<fsm0>();
+
+    return 0;
+}
+
+BOOST_MSM_BACK_GENERATE_FSM(fsm0);
