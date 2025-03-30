79a11dcb5f9be8f8137ece6781bf43e4a351d0e8
Christian Granzin <christian.granzin@gmail.com>
2025-03-30 14:38:28 -0400

    update large msm

--- bench/large/msm/Backmp11FavorCompileTime.cpp
+++ bench/large/msm/Backmp11FavorCompileTime.cpp
@@ -1,16 +1,16 @@
 #define TRANSITION_TABLE_TYPE boost::mp11::mp_list
 #include <boost/mp11.hpp>
 #include "common.hpp"
-#include <boost/msm/backmp11/state_machine.hpp>
+//
 #include <boost/msm/backmp11/favor_compile_time.hpp>
+#include <boost/msm/backmp11/state_machine.hpp>
 
 using fsm0 = msm::back::state_machine<fsm_, msm::back::favor_compile_time>;
 
-int main()
-{
-    test_fsm<fsm0>();
+int main() {
+  test_fsm<fsm0>();
 
-    return 0;
+  return 0;
 }
 
-BOOST_MSM_BACK_GENERATE_FSM(fsm0);
+BOOST_MSM_BACKMP11_GENERATE_FSM(fsm0);
